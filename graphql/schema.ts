d6e547fe9a7e3c57df8c79a5bfac1e5023e6c143
iwanbazz <iwan.bazz@gmail.com>
2020-06-13 06:42:49 +0700

    Define object and query type

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -2,3 +2,31 @@ import { schema, use } from "nexus";
 import { prisma } from "nexus-plugin-prisma";
 
 use(prisma());
+
+// Define object 'user'
+schema.objectType({
+  name: "user",
+  definition(t) {
+    t.model.id();
+    t.model.name();
+  },
+});
+
+//
+/*
+  Query a list of existing users.
+  read more about directly access 'db' prop on 'ctx' obj :
+  https://www.prisma.io/docs/reference/tools-and-interfaces/prisma-client/crud
+ */
+schema.queryType({
+  definition(t) {
+    t.list.field("allUsers", {
+      type: "user",
+      resolve(_parent, _args, ctx) {
+        return ctx.db.user.findMany();
+      },
+    });
+    t.crud.user();
+    t.crud.users();
+  },
+});
